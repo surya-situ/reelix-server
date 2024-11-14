6c4dddcb50d24501a4aa4e4b2867513de484498a
surya-situ <suryasocial55@gmail.com>
2024-11-14 15:54:10 +0530

    feat: configured the socket.io connection || fix: fixed the redis dual call error

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,22 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-import client from "./utils/client";
 import { appLimiter } from "./utils/rateLimit";
 
 dotenv.config();
 const PORT = process.env.PORT;
 
 const app: Application = express();
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+});
 
 //- GLOBAL MIDDLEWARE
 app.use(express.json());
@@ -18,13 +26,22 @@ app.use(appLimiter);
 //- VIEW ENGINE
 
 //- ROUTE
+app.get('/', function (req: Request, res: Response) {
+    res.status(200).json({
+        message: "server is running..."
+    });
+    return;
+});
+
+io.on("connection", function(socket){
+    console.log("Socket io is connected", socket);
+});
 
 async function connectServer() {
     try {
-        // await client.connect()
         console.log("Connected to Redis...");
 
-        app.listen(PORT, function() {
+        server.listen(PORT, function() {
             console.log(`Server is running on port: ${PORT}`)
         });
     } catch (error) {
