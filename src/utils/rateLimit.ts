d9206142d7534ab72065fec14b178fb843293b56
surya-situ <suryasocial55@gmail.com>
2024-11-20 14:03:01 +0530

    feat: rate limiting for auth endpoints with 20 requests per 10 minutes

--- src/utils/rateLimit.ts
+++ src/utils/rateLimit.ts
@@ -1,8 +1,19 @@
 import rateLimit from "express-rate-limit";
 
-export const appLimiter = rateLimit({
-    windowMs: 10 * 60 * 1000,
-    limit: 200,
-    standardHeaders: 'draft-7',
-    legacyHeaders: false
-});
+export const appLimiter = rateLimit(
+    {
+        windowMs: 10 * 60 * 1000,
+        limit: 200,
+        standardHeaders: 'draft-7',
+        legacyHeaders: false
+    }
+);
+
+export const authLimiter = rateLimit(
+    {
+        windowMs: 10 * 60 * 1000,
+        limit: 20,
+        standardHeaders: 'draft-7',
+        legacyHeaders: false
+    }
+);
