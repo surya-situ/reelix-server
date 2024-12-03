b0924250703d69a6b2343f6e653c9387577f760a
surya-situ <suryasocial55@gmail.com>
2024-12-03 11:38:23 +0530

    feat: add confirm password schema to the user validator

--- src/validator/userValidator.ts
+++ src/validator/userValidator.ts
@@ -9,7 +9,15 @@ export const signUpSchema = z.object(
             .max(120, { message: "Name must not be more than 120 characters long" }),
         password: z.string({ message: "Password is required" })
             .min(6, { message: "Password must be at least 6 character long" })
-            .max(60, { message: "Password must not be more than 60 character long" }),
+            .max(60, { message: "Password must not be more than 50 character long" }),
+        confirmPassword: z.string({ message: "Confirm password is required" })
+            .min(6, { message: "Password must be at least 6 character long" })
+            .max(50, { message: "Password must not be more than 50 character long" }),
+    }
+).refine((data) => data.password === data.confirmPassword,
+    {
+        message: "Password do not match",
+        path: ["confirmPassword"]
     }
 );
 
