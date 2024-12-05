f92eb9c2362b2f21054355f4c5b0ad5ce1aec102
surya-situ <suryasocial55@gmail.com>
2024-12-05 10:00:05 +0530

    feat: impliment common rate limiting configuration and apply to app, auth, password, OTP verification, and reelix duel endpoints

--- src/utils/rateLimit.ts
+++ src/utils/rateLimit.ts
@@ -1,27 +1,50 @@
 import rateLimit from "express-rate-limit";
 
+const commonRateLimitingConfig = {
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: {
+        status: 429,
+        error: "Too many requests, please try again after some time."
+    }
+};
+
 export const appLimiter = rateLimit(
     {
         windowMs: 10 * 60 * 1000,
         limit: 200,
-        standardHeaders: 'draft-7',
-        legacyHeaders: false,
-	    message: {
-        	status: 429,
-        	error: "Too many requests, please try again after some time.",
-    	}
+        ...commonRateLimitingConfig
     }
 );
 
 export const authLimiter = rateLimit(
     {
         windowMs: 10 * 60 * 1000,
-        limit: 20,
-        standardHeaders: 'draft-7',
-        legacyHeaders: false,
-	    message: {
-        	status: 429,
-        	error: "Too many attempts, please try again after some time.",
-    	}
+        limit: 10,
+        ...commonRateLimitingConfig
+    }
+);
+
+export const passwordLimiter = rateLimit(
+    {
+        windowMs: 5 * 60 * 1000,
+        limit: 3,
+        ...commonRateLimitingConfig
+    }
+);
+
+export const otpVerifyLimiter = rateLimit(
+    {
+        windowMs: 5 * 60 * 1000,
+        limit: 5,
+        ...commonRateLimitingConfig
+    }
+);
+
+export const reelixDuelLimiter = rateLimit(
+    {
+        windowMs: 5 * 60 * 1000,
+        limit: 3,
+        ...commonRateLimitingConfig
     }
 );
