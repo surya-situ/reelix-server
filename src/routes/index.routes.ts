26aa29f1d8072b6b35dfb4cef3426be14fc2783b
surya-situ <suryasocial55@gmail.com>
2024-12-08 11:48:01 +0530

    feat: add otp verification route to index route

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 
 import userRouter from "./user.routes";
+import otpRouter from "./verify.routes";
 
 const router = Router();
 
 // - user routers
 router.use( "/v1/api/user", userRouter );
+router.use("/v1/api/verify-user-email", otpRouter);
 
 export default router;
