4126ed933107c463be04005bd167aaec69eb8628
surya-situ <suryasocial55@gmail.com>
2024-12-09 10:31:22 +0530

    feat: implement middleware for JWT token verification and user authentication

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request } from "express";
+import jwt from "jsonwebtoken";
+
+import { appError } from "../utils/appError";
+
+export const authMiddleware = ( req: Request, next: NextFunction ) => {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if(!token) {
+        next(appError(400, "Access denied!"));
+        return;
+    };
+
+    try {
+        const verifyToken = jwt.verify(token, process.env.JWT_SECRET!);
+        req.user = verifyToken
+        next();
+    } catch (error) {
+        next(appError(400, 'Invalid token'));
+        return;
+    };
+};
