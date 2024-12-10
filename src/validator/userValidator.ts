3beb853d4f99c14af84d2c7103769faf7cac39e7
surya-situ <suryasocial55@gmail.com>
2024-12-10 14:19:00 +0530

    feat: zod otp validator for user's otp validation and refine confirm password schema

--- src/validator/userValidator.ts
+++ src/validator/userValidator.ts
@@ -11,8 +11,8 @@ export const signUpSchema = z.object(
             .min(6, { message: "Password must be at least 6 character long" })
             .max(60, { message: "Password must not be more than 50 character long" }),
         confirmPassword: z.string({ message: "Confirm password is required" })
-            .min(6, { message: "Password must be at least 6 character long" })
-            .max(50, { message: "Password must not be more than 50 character long" }),
+            .min(6, { message: "Confirm password must be at least 6 character long" })
+            .max(50, { message: "Confirm password must not be more than 50 character long" }),
     }
 ).refine((data) => data.password === data.confirmPassword,
     {
@@ -21,7 +21,13 @@ export const signUpSchema = z.object(
     }
 );
 
-export const loginSchema = z.object(
+export const otpSchema = z.object(
+    {
+        otp: z.string({ message: "OTP is required" }).min(6).max(6)
+    }
+);
+
+export const signinSchema = z.object(
     {
         email: z.string({ message: "Email is required" })
             .email({ message: "Invalid email address" }),
