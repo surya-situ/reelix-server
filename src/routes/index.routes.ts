081f42437f8a6ae7d383fb71203f10e6660626ad
surya-situ <suryasocial55@gmail.com>
2024-12-13 15:33:07 +0530

    feat: add reelix vidoes route for videos

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -2,11 +2,15 @@ import { Router } from "express";
 
 import userRouter from "./user.routes";
 import otpRouter from "./verify.routes";
+import videoRouter from "./reelix.routes";
 
 const router = Router();
 
-// - user routers
+// - user route
 router.use( "/v1/api/user", userRouter );
 router.use("/v1/api/verify-user-email", otpRouter);
+
+// - videos route
+router.use("/v2/api/videos", videoRouter);
 
 export default router;
