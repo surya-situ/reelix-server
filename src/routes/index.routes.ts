e4c32b4522fb27ffc07a18886ad11402f50a6e93
surya-situ <suryasocial55@gmail.com>
2024-12-18 17:06:08 +0530

    feat: impliment the forget password route

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import userRouter from "./user.routes";
 import otpRouter from "./verify.routes";
+import passwordRouter from "./password.routes";
 import videoRouter from "./reelix.routes";
 
 const router = Router();
@@ -9,6 +10,7 @@ const router = Router();
 // - user route
 router.use( "/v1/api/user", userRouter );
 router.use("/v1/api/verify-user-email", otpRouter);
+router.use("/v1/api/password", passwordRouter);
 
 // - videos route
 router.use("/v2/api/videos", videoRouter);
