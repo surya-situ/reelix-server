d29a5f78757a058ff241ab57ffa2c8f0863233d2
surya-situ <suryasocial55@gmail.com>
2024-12-19 15:13:38 +0530

    feat: impliment reset password route and remove unnecessary user data form forget password route also add comments to important code

--- src/routes/password.routes.ts
+++ src/routes/password.routes.ts
@@ -12,7 +12,6 @@ import { passwordLimiter } from "../utils/rateLimit";
 import { appError } from "../utils/appError";
 import { formatZodErrors } from "../middlewares/globalError";
 import { sendEmail } from "../utils/email/emailStore";
-import { authMiddleware } from "../middlewares/authMiddleware";
 
 const route = Router();
 const redis = new Redis();
@@ -22,6 +21,7 @@ route.post('/forget-password', passwordLimiter, async (req: Request, res: Respon
     const data = req.body;
     const payload = forgotPasswordSchema.safeParse(data);
 
+    // - If the data isn't valid according to the schema, format the error and pass it to the appError middleware
     if( !payload.success ) {
         const zodError = formatZodErrors(payload.error);
         next(appError(422, "Validation error", zodError));
@@ -31,6 +31,7 @@ route.post('/forget-password', passwordLimiter, async (req: Request, res: Respon
     try {
         const  { email }  = payload.data;
 
+        // - Find the user form the email
         let findUser = await prisma.user.findUnique(
             {
                 where: {
@@ -39,10 +40,13 @@ route.post('/forget-password', passwordLimiter, async (req: Request, res: Respon
             }
         );
    
+        // - Check for user exists or not
         if( !findUser ) {
             next(appError(401, `User with email ${email} not found`));
             return;
         };
+
+        // - Extract the name form the user to send in the email as greetings
         const { name } = findUser;
 
         const otp = randomInt(100000, 999999).toString();
@@ -63,13 +67,14 @@ route.post('/forget-password', passwordLimiter, async (req: Request, res: Respon
 
         // - Attempt to send the password reset OTP email to the user
         try {
-            await sendEmail(email, "Email Verification OTP", emailHtml);
+            await sendEmail(email, "Reset password OTP", emailHtml);
         } catch (emailError) {
             next(appError(500, "Failed to send email", emailError));
             return;
         };
 
-        const updateUserData = await prisma.user.update(
+        // - Updating user's password otp and expire time
+        await prisma.user.update(
             {
                 where: {
                     email
@@ -81,12 +86,12 @@ route.post('/forget-password', passwordLimiter, async (req: Request, res: Respon
             }
         );
 
+
         res.status(200).json(
             {
                 status: "success",
                 message: "Password reset OTP sent to email",
-                data: updateUserData,
-                token: tempToken
+                token: tempToken as string
             }
         );
         return;
@@ -97,5 +102,71 @@ route.post('/forget-password', passwordLimiter, async (req: Request, res: Respon
     };
 });
 
+
+route.post('/reset-password', passwordLimiter, async (req: Request, res: Response, next: NextFunction) => {
+    const data = req.body;
+    const payload = resetPasswordSchema.safeParse(data);
+    const tempToken = req.headers['authorization']?.split(' ')[1];
+
+    // - If the data isn't valid according to the schema, format the error and pass it to the appError middleware
+    if( !payload.success ) {
+        const zodError = formatZodErrors(payload.error);
+        next(appError(422, "Validation error", zodError));
+        return;
+    };
+    
+    // - Check for missing token
+    if ( !tempToken ) {
+        next(appError(422, "No token is provided"));
+        return;
+    };
+
+    try {
+        // - Decode the temporary token to get the user's email
+        const decode = jwt.verify( tempToken, process.env.JWT_SECRET!) as { email: string };
+
+        const { otp, password } = payload.data;
+
+        // - Encrypt password;
+        const encryptPassword = await bcrypt.hash(password, 10);
+
+        // - Get the OTP stored in the Redis
+        const storedOtp = await redis.get( decode.email );
+
+        // - Check for invalid or expired OTP
+        if( !storedOtp || storedOtp !== otp ) {
+            next(appError(401, "Invalid or expired OTP"));
+        };
+
+        // - Update user password and reset password related otp expire to null
+        await prisma.user.update(
+            {
+                where: {
+                    email: decode.email
+                },
+                data: {
+                    password: encryptPassword,
+                    password_reset_otp: null,
+                    reset_otp_expire_at: null
+                }
+            }
+        );
+
+        // - Delete otp from the redis queue
+        await redis.del(decode.email);
+
+        res.status(200).json(
+            {
+                status: "Success",
+                message: "Password update successfully"
+            }
+        );
+        return;
+
+    } catch (error) {
+        next(appError( 500, "Something went wrong."));
+        return;
+    }
+});
 
 export default route;
